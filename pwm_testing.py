07162e9c66582c2eb6061084131cf5294674812f
John Thorne <john.thorne@John-Thorne-MBP.local>
2020-01-29 16:25:15 -0800

    test

--- pwm_testing.py
+++ pwm_testing.py
@@ -13,9 +13,9 @@ for i in pins:
 	GPIO.output(pins[i], GPIO.HIGH) # Set pins to high(+3.3V) to off led
 
 print ("Freuency 1 KHz")
-p_R = GPIO.PWM(pins['pin_R'], 1000)  # set Frequece to 2KHz
-p_G = GPIO.PWM(pins['pin_G'], 1000)
-p_B = GPIO.PWM(pins['pin_B'], 1000)
+p_R = GPIO.PWM(pins['pin_R'], 100)  # set Frequece to 2KHz
+p_G = GPIO.PWM(pins['pin_G'], 100)
+p_B = GPIO.PWM(pins['pin_B'], 100)
 
 p_R.start(0)      # Initial duty Cycle = 0(leds off)
 p_G.start(0)
