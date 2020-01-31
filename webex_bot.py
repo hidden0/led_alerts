f5f3ffabb85690f83eb93a1f55c0f7d29d2a7615
John Thorne <john.thorne@John-Thorne-MBP.local>
2020-01-31 14:47:47 -0800

    test

--- webex_bot.py
+++ webex_bot.py
@@ -74,12 +74,12 @@ for item in json_data['items']:
 	matches = re.finditer(regex, message, re.MULTILINE)
 
 	for matchNum, match in enumerate(matches, start=1):
-		print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-
-		for groupNum in range(0, len(match.groups())):
+		#print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+		print (match)
+		"""for groupNum in range(0, len(match.groups())):
 			groupNum = groupNum + 1
 
-			print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+			print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))"""
 exit()
 try:
 	#we are starting with the loop
