bca036e36ad576a6c86fe6ffd96e5d26eddfa8ff
John Thorne <john.thorne@John-Thorne-MBP.local>
2020-01-31 14:54:19 -0800

    test

--- webex_bot.py
+++ webex_bot.py
@@ -65,6 +65,7 @@ fullQuery = apiUrl + apiAction + params
 m = {'roomId': roomId.rstrip(), 'mentionedPeople': daveId.rstrip()}
 r = requests.get('https://api.ciscospark.com/v1/messages?max=1&roomId=Y2lzY29zcGFyazovL3VzL1JPT00vYjk4MWU5NzAtNDQ3Ni0xMWVhLWI2YjctMTEzZjlmN2YyOTQy&mentionedPeople=me', headers={'Authorization': 'Bearer '+str(daveToken).rstrip()})
 json_data = json.loads(r.text)
+selectedColor = "green"
 
 for item in json_data['items']:
 	message = item['text']
