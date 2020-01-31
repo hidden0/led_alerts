ace2f64f3a8b3286aed736ab26c0dec3df8cb6d7
John Thorne <john.thorne@John-Thorne-MBP.local>
2020-01-30 16:58:19 -0800

    testes

--- webex_bot.py
+++ webex_bot.py
@@ -40,27 +40,27 @@ try:
 		BLUE.start(1)
 		#For anode RGB LED users, if you want to start with RED too the only thing to be done is defining RED as one and GREEN and BLUE as 100.
 
-			for x in range(1,101):
-				# for changing the width of PWM, this command is used
-				GREEN.ChangeDutyCycle(x)
- 				#for anode LED users, just change x with 101-x
+		for x in range(1,101):
+			# for changing the width of PWM, this command is used
+			GREEN.ChangeDutyCycle(x)
+				#for anode LED users, just change x with 101-x
 
-				# and for delay time.sleep is used. You can chance the duration of the colors with changing the time from here
-				time.sleep(0.05)
+			# and for delay time.sleep is used. You can chance the duration of the colors with changing the time from here
+			time.sleep(0.05)
 
-			for x in range(1,101):
+		for x in range(1,101):
 
-				RED.ChangeDutyCycle(101-x)
-				time.sleep(0.025)
+			RED.ChangeDutyCycle(101-x)
+			time.sleep(0.025)
 
-			for x in range(1,101)
-				GREEN.ChangeDutyCycle(101-x)
-				BLUE.ChangeDutyCycle(x)
-				time.sleep(0.025)
+		for x in range(1,101)
+			GREEN.ChangeDutyCycle(101-x)
+			BLUE.ChangeDutyCycle(x)
+			time.sleep(0.025)
 
-			for x in range(1,101):
-				RED.ChangeDutyCycle(x)
-				time.sleep(0.025)
+		for x in range(1,101):
+			RED.ChangeDutyCycle(x)
+			time.sleep(0.025)
 
 	except KeyboardInterrupt:
 		# the purpose of this part is, when you interrupt the code, it will stop the while loop and turn off the pins, which means your LED won't light anymore
