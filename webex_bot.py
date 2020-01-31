48498fc6a976944a1292159c64664bbeee2f2351
John Thorne <john.thorne@John-Thorne-MBP.local>
2020-01-31 14:58:01 -0800

    asdf

--- webex_bot.py
+++ webex_bot.py
@@ -105,7 +105,7 @@ try:
 
 			for matchNum, match in enumerate(matches, start=1):
 				#print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
-				setColor(colors[(match.group(0))])
+				setColor(colors[(match.group(0).lower())])
 
 		time.sleep(5)
 
