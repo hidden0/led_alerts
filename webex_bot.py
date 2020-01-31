f990d201d52923e0b1c7682d1f990b1510afb5f1
John Thorne <john.thorne@John-Thorne-MBP.local>
2020-01-31 14:19:59 -0800

    test

--- webex_bot.py
+++ webex_bot.py
@@ -60,7 +60,7 @@ roomId = (f.read())
 f.close()
 
 fullQuery = apiUrl + apiAction + params
-ans = requests.post(fullQuery)
+ans = requests.post(fullQuery, headers={'Authorization': daveToken)
 
 print(ans.text)
 exit()
