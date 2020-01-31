128a48e5f9ff733fc34bc2e1bfea847d8faaea36
John Thorne <john.thorne@John-Thorne-MBP.local>
2020-01-31 14:46:27 -0800

    asdf

--- webex_bot.py
+++ webex_bot.py
@@ -69,11 +69,17 @@ json_data = json.loads(r.text)
 for item in json_data['items']:
 	message = item['text']
 	print(message)
-	matches = re.match("\b(?:red|blue|green|yellow|orange|purple|white)\b", message)
-	if not matches:
-		continue
-	print(matches)
-	print(str(matches.group(0)))
+	regex = r"\b(?:red|blue|green|yellow|orange|purple|white)\b"
+
+	matches = re.finditer(regex, message, re.MULTILINE)
+
+	for matchNum, match in enumerate(matches, start=1):
+		print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+
+		for groupNum in range(0, len(match.groups())):
+			groupNum = groupNum + 1
+
+			print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
 exit()
 try:
 	#we are starting with the loop
